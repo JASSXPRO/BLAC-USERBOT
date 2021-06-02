06cbd805ca9de92bdd19c7707744c35d9cb4e434
TEAM-BLAC <85191164+TEAM-BLAC@users.noreply.github.com>
2021-06-02 13:25:05 +0000

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -23,8 +23,8 @@ async def send(event):
     message_id = event.message.id
     thumb = SAVAGE_logo
     input_str = event.pattern_match.group(1
-    omk = f"𝖯𝙻𝚄𝙶𝙸𝙽 𝙽𝙰𝙼𝙴 ➪ `{input_str}`\n𝖴𝙿𝙻𝙾𝙰𝙳𝙴𝙳 𝙱𝚈 ➪ {SAVAGE}\n\n[blac userbot ©](https://github.com/sameerpanthi/BLAC-2.0-USERBOT)"
-    the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
+    omk = f"𝖯𝙻𝚄𝙶𝙸𝙽 𝙽𝙰𝙼𝙴 ➪ `{input_str}`\n𝖴𝙿𝙻𝙾𝙰𝙳𝙴𝙳 𝙱𝚈 ➪ {mafia}\n\n[blac 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ©](t.me/blac_userbot)"
+  the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
         lauda = await event.client.send_file(
             event.chat_id,
